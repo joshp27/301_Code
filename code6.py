1187b969767a2d7b9eb654e55ccd6c71b466cc01
joshp27 <joshphipps2@aol.com>
2023-03-21 02:09:24 +0000

    This is my code challenge 6 (python)

---
+++ code6.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3 
+# Snoop_Dog = os.system("whoami")
+
+# os.system("ip a")
+# var =  ["classmates"]
+
+#     print("whoami")
+
+# var =  ["("ip a")]
+# for item in var:
+#     print("ip a")  
+
+# var =  ["os.system("lshw -short")]
+# for item in var:
+#    print("lshw -short"))
+
+
+
+
+
+import os
+
+My_name = os.system("whoami")
+print(My_name)
+
+Address = os.system("ip a")
+print(Address)
+
+extract = os.system("lshw -short")
+print(extract)
