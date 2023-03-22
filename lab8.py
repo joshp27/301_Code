fe53a4927708b09cdc988a755f1b41d77411d919
joshp27 <joshphipps2@aol.com>
2023-03-22 22:57:29 +0000

    This is my Code8 X-Men Collection

---
+++ lab8.py
@@ -0,0 +1,19 @@
+# # Script: Ops 301 Class 08 Code Challenge
+# # Joshua Phipps
+# # 3/22/2023
+# # Purpose: Python Collections
+
+#!/usr/bin/env python3
+
+
+
+# Assign to a variable a list of ten string elements.
+thislist = ["Storm", "Jean Grey", "Jubilee", "Psylocke", "Iceman", "Emma Frost", "Magneto", "Scarlet Witch", "Bishop", "Nightcrawler"]
+print(thislist)
+# Print the fourth element of the list.
+print(thislist[3])
+# Print the sixth through tenth element of the list.
+print(thislist[5:10])
+# Change the value of the seventh element to “onion”.
+thislist[6] = "Onion"
+print(thislist)
